f277f43918609e9e984e3f32506b89df52d5528b
Priyangshu <pjscse@gmail.com>
2023-02-09 10:22:22 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App1());
+}
+
+class App1 extends StatelessWidget {
+  const App1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Material(
+        child: Center(
+          child: Container(
+            child: Text("Welcome to the APP ONE!!"),
+          ),
+        ),
+      ),
+    );
+  }
+}
