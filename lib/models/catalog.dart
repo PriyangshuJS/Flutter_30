f403cf2abb539bf0773f09977ae3ea670f034fe8
Priyangshu <pjscse@gmail.com>
2023-02-24 09:27:58 +0530

    10th Commit

---
+++ lib/models/catalog.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class item {
+  final num id;
+  final String name;
+  final String desc;
+  final num price;
+  final String colour;
+  final String image;
+
+  item(this.id, this.name, this.desc, this.price, this.colour, this.image);
+}
