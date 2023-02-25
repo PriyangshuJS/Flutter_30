bee9613f5311b2d86cc46b1625949ef764c3d2d5
Priyangshu <pjscse@gmail.com>
2023-02-25 21:12:28 +0530

    15 and 14 in huge confusion

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -2,10 +2,29 @@ import 'package:app1/models/catalog.dart';
 import 'package:app1/widgets/drawer.dart';
 import 'package:flutter/material.dart';
 import 'package:app1/widgets/item.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 
-class Homepage extends StatelessWidget {
-  var nu = 1;
-  String s = "Learning from Codepur.";
+class Homepage extends StatefulWidget {
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  void initState() {
+    super.initState();
+    LoadData();
+  }
+
+  LoadData() async {
+    var CatalogJson = await rootBundle.loadString("assets/Files/catalog.json");
+    var DecodeData = jsonDecode(CatalogJson);
+    var ProductData = DecodeData["products"];
+    CatalogModel.items =
+        List.from(ProductData).map<Item>((item) => Item.fromMap(item)).toList();
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
